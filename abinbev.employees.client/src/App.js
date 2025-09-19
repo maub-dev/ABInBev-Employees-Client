b1707f904325189b52c5a1a4b281b3808a642d35
maub-dev <mau.boff@gmail.com>
2025-09-19 08:55:21 -0300

    Project creation

---
+++ abinbev.employees.client/src/App.js
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
